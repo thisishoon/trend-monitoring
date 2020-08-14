b9f78c10ad5c567ee91195f937dd94f413fbdf61
thisishoon <jihoon522@naver.com>
2020-08-14 16:54:15 +0900

    add circle ci & remove function of extracts keyword using frequency

--- monitering/extract.py
+++ monitering/extract.py
@@ -4,34 +4,6 @@ from collections import Counter
 from krwordrank.word import KRWordRank, summarize_with_keywords
 
 
-def extract_keyword_frequency(word, news_links):
-    okt = Okt()
-    result = ""
-    for news_link in news_links:
-        article = Article(news_link, language='ko')
-        article.download()
-        try:
-            article.parse()
-            content = article.text
-            result += content
-        except ArticleException as ae:
-            continue
-
-    nouns = okt.nouns(result)
-
-    nouns_over_2char = []
-    for noun in nouns:
-        if len(noun) < 2 or noun == word:
-            continue
-        else:
-            nouns_over_2char.append(noun)
-
-    count = Counter(nouns_over_2char).most_common(10)
-    related_keyword = [i[0] for i in count]
-
-    return related_keyword
-
-
 def extract_keyword_textrank(input_list):
     min_count = 5
     max_length = 10
@@ -79,6 +51,6 @@ def make_news_contents(search_word, news_links):
                 elif morph in ['NNP', 'NNG', 'NP'] and len(word) > 1:
                     news_content.append(word)
 
-        news_contents.append(" ".join(news_content))
+        news_contents.append(news_content)
 
     return news_contents
