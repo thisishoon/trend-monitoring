533ee1cec5ed973d31dd6ee2bf3f473c044c3184
thisishoon <jihoon522@naver.com>
2020-08-22 11:33:19 +0900

    code review 5 - integration of check_person, movie and tv fuctions into single function of check_category & declare html element string for bs4 as global variable

--- tremo/check.py
+++ tremo/check.py
@@ -6,9 +6,16 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) \
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 }
+
 URL_CATEGORY = 'https://search.naver.com/search.naver?where=nexearch&sm=tab_jum&query='
+
 NAME_MOVIE = '영화'
 NAME_UNKNOWN = 'unknown'
+
+ELEMENT_CATEGORY_PERSON = '#people_info_z > div.cont_noline > div > dl > dd.name > span '
+ELEMENT_CATEGORY_MOVIE = '#_au_movie_info > div.section_head > h2'
+ELEMENT_CATEGORY_TV = 'div.broadcast_detail > div.top_info > dl > dd '
+ELEMENT_RELATED_SEARCH_WORD = '#nx_related_keywords > dl > dd.lst_relate._related_keyword_list > ul li'
 
 
 def check_category(item):
@@ -16,17 +23,29 @@ def check_category(item):
     soup = BeautifulSoup(res.text, 'html.parser')
 
     category = []
-    person_result = check_person(soup)
-    movie_result = check_movie(soup)
-    tv_result = check_tv(soup)
 
-    if person_result:
-        category += person_result
-    elif movie_result:
-        category.append(NAME_MOVIE)
-    elif tv_result:
-        category.append(tv_result)
+    try:
+        query = soup.select_one(ELEMENT_CATEGORY_PERSON).text
+    except AttributeError:
+        pass
     else:
+        category += query.split(', ')
+
+    try:
+        soup.select_one(ELEMENT_CATEGORY_MOVIE).text
+    except AttributeError:
+        pass
+    else:
+        category.append(NAME_MOVIE)
+
+    try:
+        query = soup.select_one(ELEMENT_CATEGORY_TV).text.strip()
+    except AttributeError:
+        pass
+    else:
+        category.append(query)
+
+    if not category:
         category.append(NAME_UNKNOWN)
 
     related_search = check_related_search(soup)
@@ -34,35 +53,8 @@ def check_category(item):
     return category, related_search
 
 
-def check_person(soup):
-    try:
-        query = soup.select("#people_info_z > div.cont_noline > div > dl > dd.name > span ")[-1].text
-    except Exception as e:
-        return False
-    else:
-        return query.split(', ')
-
-
-def check_movie(soup):
-    try:
-        query = soup.select_one("#_au_movie_info > div.section_head > h2").text
-    except Exception as e:
-        return False
-    else:
-        return True
-
-
-def check_tv(soup):
-    try:
-        query = soup.select_one("div.broadcast_detail > div.top_info > dl > dd ").text.strip()
-    except Exception as e:
-        return False
-    else:
-        return query
-
-
 def check_related_search(soup):
-    related_search = soup.select("#nx_related_keywords > dl > dd.lst_relate._related_keyword_list > ul li")
+    related_search = soup.select(ELEMENT_RELATED_SEARCH_WORD)
     result = []
 
     if related_search:
